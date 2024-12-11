72c13fb4bbb288a2c43dcff85994cd6074aa6dbb
ivantrywicaksono <ivantrywicaksono04@gmail.com>
2024-12-11 18:45:33 +0700

    feat: dynamic ResultBoard mode

--- app/src/main/java/id/handlips/component/board/ResultBoard.kt
+++ app/src/main/java/id/handlips/component/board/ResultBoard.kt
@@ -11,30 +11,59 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Call
-import androidx.compose.material.icons.filled.Refresh
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import id.handlips.R
 import id.handlips.component.button.CircleButton
 import id.handlips.ui.theme.Blue
 import id.handlips.ui.theme.Red
 import id.handlips.ui.theme.White
+import id.handlips.views.translator.SpeechState
 import id.handlips.views.translator.TranslatorMode
 
 @Composable
 fun ResultBoard(
+    modifier: Modifier = Modifier,
     result: String,
     translatorMode: TranslatorMode = TranslatorMode.HAND_SIGN,
+    speechState: SpeechState = SpeechState.STOPPED,
+    onClickTranslatorMode: () -> Unit,
+    onSpeechButtonClick: () -> Unit,
 ) {
+    val speechContainerColor: Color =
+        when (speechState) {
+            SpeechState.RECORDING -> White
+            SpeechState.STOPPED -> Red
+        }
+
+    val speechContentColor: Color =
+        when (speechState) {
+            SpeechState.RECORDING -> Color.Black
+            SpeechState.STOPPED -> White
+        }
+
+    val speechStateIcon: Painter =
+        when (speechState) {
+            SpeechState.RECORDING -> painterResource(id = R.drawable.round_stop_24)
+            SpeechState.STOPPED -> painterResource(id = R.drawable.ic_mic)
+        }
+    val translatorModeIcon: Painter =
+        when (translatorMode) {
+            TranslatorMode.HAND_SIGN -> painterResource(id = R.drawable.ic_mic)
+            TranslatorMode.SPEECH -> painterResource(id = R.drawable.ic_camera)
+        }
+
     Card(
+        modifier = modifier,
         shape =
             RoundedCornerShape(
                 topStart = 20.dp,
@@ -67,10 +96,10 @@ fun ResultBoard(
                 when (translatorMode) {
                     TranslatorMode.SPEECH -> {
                         CircleButton(
-                            icon = Icons.Filled.Star,
-                            containerColor = Red,
-                            contentColor = White,
-                            onClick = {},
+                            icon = speechStateIcon,
+                            containerColor = speechContainerColor,
+                            contentColor = speechContentColor,
+                            onClick = onSpeechButtonClick,
                             contentDescription = "",
                             size = 120,
                         )
@@ -85,14 +114,14 @@ fun ResultBoard(
                     horizontalArrangement = Arrangement.SpaceBetween,
                 ) {
                     CircleButton(
-                        icon = Icons.Filled.Refresh,
+                        icon = painterResource(id = R.drawable.ic_history),
                         onClick = { null },
                         contentDescription = "Riwayat Percakapan",
                     )
                     CircleButton(
-                        icon = Icons.Filled.Call,
-                        onClick = { null },
-                        contentDescription = "Terjemah Suara ke Teks",
+                        icon = translatorModeIcon,
+                        onClick = onClickTranslatorMode,
+                        contentDescription = "Translator Mode",
                     )
                 }
             }
